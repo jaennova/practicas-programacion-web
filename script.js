41e09ccc85633df3c77c3b60e2727b4efb0f7309
Jaen <jaennova9@gmail.com>
2023-09-18 13:57:49 -0600

    project update

--- script.js
+++ script.js
@@ -6,3 +6,39 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 });
+///////
+document.addEventListener("DOMContentLoaded", function() {
+    var links = document.querySelectorAll("a");
+
+    function mostrarFecha(e, fecha) {
+        var tooltip = document.createElement("div");
+        tooltip.className = "tooltip";
+        tooltip.textContent = "Fecha: " + fecha;
+
+        document.body.appendChild(tooltip);
+
+        var offsetX = 10;
+        var offsetY = 10;
+
+        tooltip.style.left = (e.pageX + offsetX) + "px";
+        tooltip.style.top = (e.pageY + offsetY) + "px";
+
+        setTimeout(function() {
+            tooltip.remove();
+        }, 2000);
+    }
+
+    links.forEach(function(link) {
+        link.addEventListener("mouseover", function(e) {
+            var fecha = this.getAttribute("data-fecha");
+            mostrarFecha(e, fecha);
+        });
+
+        link.addEventListener("mouseout", function() {
+            var tooltips = document.querySelectorAll(".tooltip");
+            tooltips.forEach(function(tooltip) {
+                tooltip.remove();
+            });
+        });
+    });
+});
