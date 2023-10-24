33b14bd49a99838b328673ba2262016014d3a2f1
Jaen <jaennova9@gmail.com>
2023-10-24 09:37:38 -0600

    funciones

--- practicas/unidad-3/funciones/script.js
+++ practicas/unidad-3/funciones/script.js
@@ -1,13 +1,15 @@
 function suma(num1, num2) {
-    return num1 + num1
+    return num1 + num2; // Cambiado de num1 + num1 a num1 + num2
 }
-function saludo(nombre) {
-    document.write("saludos " + nombre);
-}
-function calcularArea(base, altura) {
 
+function saludo(nombre) {
+    document.write("Saludos " + nombre); // Corregido el mensaje de saludo
+}
+
+function calcularArea(base, altura) {
+    return base * altura; // Añadida la implementación para calcular el área
 }
 
 function texto(texto, color, tamanio) {
-    document.write("<font color =" + color + "size = " + tamanio + "/>" + texto);
+    document.write('<font color="' + color + '" size="' + tamanio + '">' + texto + '</font>'); // Corregido el formato de la etiqueta de fuente
 }
