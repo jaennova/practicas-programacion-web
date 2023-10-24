ea981fa759c1801ee8ae8f9310390b04508bbe1b
Jaen <jaennova9@gmail.com>
2023-10-24 09:42:43 -0600

    update

--- practicas/unidad-3/date/script.js
+++ practicas/unidad-3/date/script.js
@@ -14,11 +14,27 @@ function obtenerHora() {
     return `${horas}:${minutos}:${segundos}`;
 }
 
+function obtenerSaludo() {
+    const hora = new Date().getHours();
+    let saludo;
+
+    if (hora >= 5 && hora < 12) {
+        saludo = "Buenos días";
+    } else if (hora >= 12 && hora < 18) {
+        saludo = "Buenas tardes";
+    } else {
+        saludo = "Buenas noches";
+    }
+
+    return saludo;
+}
+
 function saludarConFechaYHora() {
     const fecha = obtenerFecha();
     const hora = obtenerHora();
-    const saludo = `¡Hola! Hoy es ${fecha} y son las ${hora}.`;
-    document.getElementById("saludo").innerHTML = saludo;
+    const saludo = obtenerSaludo();
+    const mensaje = `¡${saludo}! Hoy es ${fecha} y son las ${hora}.`;
+    document.getElementById("saludo").innerHTML = mensaje;
 }
 
 document.getElementById("boton").addEventListener("click", saludarConFechaYHora);
